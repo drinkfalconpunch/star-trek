fb9f2b8b77a3c0c3820d0eabde4196489ad24bc9
John Hudson <drinkfalconpunch@me.com>
2019-05-28 13:48:47 -0500

    started jupyter notebook. have basic scraping built.

---
+++ episode.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
+class Episode:
+    series_name: str
+    season_number: int
+    episode_number: int
+    episode_name: str
+    multi_part: bool
+    script_file_type: str

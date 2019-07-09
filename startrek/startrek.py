a8ae0cf0b01f5a10de24cbde36434bceac3b21ef
ef <2425066+drinkfalconpunch@users.noreply.github.com>
2019-07-09 15:20:47 -0500

    changed some attribute names in StarTrek

--- startrek/startrek.py
+++ startrek/startrek.py
@@ -30,9 +30,9 @@ class StarTrek:
             "imdb_movie_number": 106145
         }
     }
-    _series_short = {'movies', 'ds9', 'tng', 'tos', 'voyager', 'enterprise'}
+    _series_short_names = {'movies', 'ds9', 'tng', 'tos', 'voyager', 'enterprise'}
     _full_name = "Star Trek - {}"
-    _series_names = {
+    _series_long_names = {
         "movies":     "Movies",
         "tng":        "The Next Generation",
         "tos":        "The Original Series",
@@ -42,52 +42,58 @@ class StarTrek:
     }
     _movie_names = {}
 
-    def __init__(self, series: str):
-        if series not in self._series_short:
-            raise ValueError(f'Invalid series {series}. {self._series_short}')
-        self._series = series
-        self.full_name = self._full_name.format(self._series_names[series])
-        self.spider = StarTrekSpider(series=series)
-        self.episodes = None
+    def __init__(self, series_short_name: str):
+        if series_short_name not in self._series_short_names:
+            raise ValueError(f'Invalid series {series_short_name}. {self._series_short_names}')
+        self._series_short_name = series_short_name
+        self.full_name = self._full_name.format(self._series_long_names[series_short_name])
+        self.spider = StarTrekSpider(series=series_short_name)
+        self.series = None
 
     def __str__(self):
-        return self._full_name.format(self._series_names[self._series])
+        return self._full_name.format(self._series_long_names[self.series_short_name])
 
     def __repr__(self):
-        return f'<class startrek.StarTrek, series: {self._series_names[self._series]}>'
+        return f'<class startrek.StarTrek, series: {self._series_long_names[self.series_short_name]}>'
 
     @property
-    def series(self):
-        return self._series
+    def series_short_name(self):
+        return self._series_short_name
 
-    @series.setter
-    def series(self, var):
-        if var not in self._series_short:
-            raise ValueError(f'Invalid series {var}. {self._series_short}')
-        del self.episodes
-        self._series = var
-        self.full_name = self._full_name.format(self._series_names[var])
+    @series_short_name.setter
+    def series_short_name(self, var):
+        if var not in self._series_short_names:
+            raise ValueError(f'Invalid series {var}. {self._series_short_names}')
+        del self.series
+        self._series_short_name = var
+        self.full_name = self._full_name.format(self._series_long_names[var])
 
     def download_scripts(self, folder=None):
         self.spider.download_scripts(folder)
 
     def download_imdb_episodes(self):
-        if self.episodes:
-            return self.episodes
+        if self.series:
+            return self.series
 
         print('Downloading episodes...')
         ia = IMDb()
         # show = ia.search_movie(self.full_name)[0]
         # imdbID = ia.get_imdbID(show)
-        long_name = self._series_names[self._series]
+        long_name = self._series_long_names[self._series_short_name]
         imdbID = self._star_trek[long_name]['imdb_movie_number']
         show = ia.get_movie(imdbID)
 
         # Gets all episodes Dict[season, list[episodes]]
         ia.update(show, 'episodes')
 
-        self.episodes = Series(show['episodes'])
+        self.series = Series(show['episodes'])
         print('Finished populating episodes.')
 
     def get_episode(self, season: int, episode: int):
-        return self.episodes.get_episode(season, episode)
+        return self.series.get_episode(season, episode)
+
+    def set_script(self, season, episode, script_path=None, script_text=None):
+        if not self.series:
+            print('Episodes not populated. Use download_imdb_episodes first.')
+            return
+        self.series.season[season].episode[episode].set_script(script_path, script_text)

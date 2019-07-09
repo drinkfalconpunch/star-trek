a068a04f0385ebf8802060c8db98ac234eeba5ee
John Hudson <drinkfalconpunch@me.com>
2019-07-09 14:29:17 -0500

    added dict_search function fragment

--- startrek/utils.py
+++ startrek/utils.py
@@ -3,3 +3,9 @@ from collections import OrderedDict
 def sorted_dict(input):
     # Returns a dictionary sorted by keys.
     return OrderedDict(sorted(input.items()))
+
+def dict_search(d, value):
+    if not isinstance(d, dict):
+        raise ValueError('dict_search requires dictionary')
+
+    pass

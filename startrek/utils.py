da243e806e374c9404fd7c2f3f9129179a434191
ef <2425066+drinkfalconpunch@users.noreply.github.com>
2019-07-12 21:48:32 -0500

    got sectioned script working

--- startrek/utils.py
+++ startrek/utils.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+from itertools import tee
 
 def sorted_dict(input):
     # Returns a dictionary sorted by keys.
@@ -9,3 +10,9 @@ def dict_search(d, value):
         raise ValueError('dict_search requires dictionary')
 
     pass
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
